73bd83024e7194cb562df8e2bb227ef1c2ee55c0
Luca-Mariselli <lucamariselli@gmail.com>
2024-05-01 21:55:15 +0200

    M4 - Creo funzioni per creazione quadrati ed eventlistener

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
+// creo funzione per quadrati
+function creaQuad(numero){
+    quadrato = document.createElement(`div`);
+    quadrato.classList.add(`quadrato`)
+    quadrato.innerText = numero
+    griglia.append(quadrato)
+}
 
-
+// creo funzione per eventlistener
+function cliccati(elemento, numero){
+    elemento.addEventListener(`click`, function () {
+        console.log(`hai cliccato sul quadrato ${numero}`)
+        this.classList.toggle(`background`)
+    })
+}
 
 // creo ciclo per iniettare quadrati in pagina
 let griglia = document.getElementById(`griglia`)
 let quadrato;
 
 for (let i=1; i <= 100; i++) {
-    quadrato = document.createElement(`div`);
-    quadrato.classList.add(`quadrato`)
-    quadrato.innerText = i
-
-    // aggiungo eventlistener 
-    quadrato.addEventListener(`click`, function () {
-        console.log(`hai cliccato sul quadrato ${i}`)
-        this.classList.toggle(`background`)
-    })
-
-    griglia.append(quadrato)
-    
+    let creazione = creaQuad(i)
+    let aggClasse = cliccati(quadrato, i)
+    // // aggiungo eventlistener 
+    // quadrato.addEventListener(`click`, function () {
+    //     console.log(`hai cliccato sul quadrato ${i}`)
+    //     this.classList.toggle(`background`)
+    // })
 }
 
