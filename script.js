936ba0613045f75bea53eadef3ae4f0b641af815
Luca-Mariselli <lucamariselli@gmail.com>
2024-05-01 21:47:27 +0200

    M3 - Aggiunto eventlistener

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+
+
+
 // creo ciclo per iniettare quadrati in pagina
 let griglia = document.getElementById(`griglia`)
+let quadrato;
 
 for (let i=1; i <= 100; i++) {
-    let quadrato = document.createElement(`div`);
+    quadrato = document.createElement(`div`);
     quadrato.classList.add(`quadrato`)
-    griglia.append(quadrato)
     quadrato.innerText = i
+
+    // aggiungo eventlistener 
+    quadrato.addEventListener(`click`, function () {
+        console.log(`hai cliccato sul quadrato ${i}`)
+        this.classList.toggle(`background`)
+    })
+
+    griglia.append(quadrato)
+    
 }
+
